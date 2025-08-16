8722f4666414cb3028bbf0b0de9a5b1cdeadd856
Dainover <jimenezelkin02@gmail.com>
2025-08-15 21:29:08 -0500

    .

---
+++ Backend/src/main/java/com/GestorDeCitas/Backend/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.GestorDeCitas.Backend.repository;
+
+import java.util.Optional;
+
+import com.GestorDeCitas.Backend.models.Roles.ERole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.GestorDeCitas.Backend.models.Roles.Roles;
+
+public interface RoleRepository extends JpaRepository <Roles, Integer> {
+    Optional<Roles> findByRole(ERole role);
+}
---
+++ Backend/src/main/java/com/GestorDeCitas/Backend/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.GestorDeCitas.Backend.repository;
+
+import com.GestorDeCitas.Backend.models.Roles.ERole;
+import com.GestorDeCitas.Backend.models.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByUsername(String username);
+    Optional<Users> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+
+    boolean existsByRoles_Role(ERole role);
+    List<Users> findByRoles_Role(ERole role);
+}
